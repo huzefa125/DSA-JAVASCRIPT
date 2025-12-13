d2909402a66403f229020ca46fd8e85ef7bd7b14
Shaikh Huzef <173603238+huzefa125@users.noreply.github.com>
2025-12-13 13:35:43 +0530

    Add files via upload

---
+++ basics java/dowhileloops.java
@@ -0,0 +1,10 @@
+package loops;
+public class dowhileloops {
+    public static void main(String args[]){
+        int i = 1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i <= 10);
+    }
+}
